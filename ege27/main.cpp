20f084c533aa4f159ad5c7c3a49a7a07da9d27d8
AlpatovGleb <alpatov31@yandex.ru>
2021-11-09 12:08:51 +0300

    effective solution ready

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -11,17 +11,22 @@ void number_of_products_div() /// number of products dividable by smth
     3. ответ n55*n11 + n55*n5 + n5*no + n5*n11
     */
     int n(0), cur(0);
+    long n15(0), n3(0), n5(0), no(0);
     cin >> n;
     for (int i(0); i<n; i++)
     {
         cin >> cur;
+        if (cur%15 == 0) n15++;
+        else if (cur%3==0) n3++;
+        else if (cur%5==0) n5++;
     }
-
+    no = n - n15 - n5 - n3;
+    cout << n15*n3 + n15*n5 + n15 * no + n15 * (n15-1)/2 + n5*n3 << endl;
 }
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_products_div();
     return 0;
 }
