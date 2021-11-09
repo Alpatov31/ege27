92b1343ee0255c68efa18ff7dc3e209446cc398c
AlpatovGleb <alpatov31@yandex.ru>
2021-11-09 11:58:51 +0300

    reads numbers

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 void number_of_products_div() /// number of products dividable by smth
 {
-    cout << "Hello, world!" << endl;
+    /**
+    1. считать число
+    2. добавить его к одному из счетчиков соответсвующего класса эквивалентности:
+        делится на 55, делится на 5 не на 11, делится на 11 н на 5, не делится ни на 5 ни на 11
+    3. ответ n55*n11 + n55*n5 + n5*no + n5*n11
+    */
+    int n(0), cur(0)
+    cin >> n;
+    for (int i(0); i<n; i++)
+    {
+        cin >> cur
+    }
+
 }
 
 
