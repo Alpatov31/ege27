d31801c4af35d16e05d0944b83dc4bcfd246ed80
AlpatovGleb <alpatov31@yandex.ru>
2021-11-09 10:48:58 +0300

    added development plan

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -5,6 +5,14 @@ using namespace std;
 void number_of_products_div() /// number of products dividable by smth
 {
     cout << "Hello, world!" << endl;
+    /**
+    1. считать и запомнить числа
+    2. вывести все пары чисел
+    3. вместо пар вывести произведения
+    4. проверить делимость произведений
+    5. посчитать и вывести количество
+        подходящих произведений
+    */
 }
 
 
