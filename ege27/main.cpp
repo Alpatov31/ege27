4325f59bac1c11cdb995095f2739672fc5a62cb3
AlpatovGleb <alpatov31@yandex.ru>
2021-11-09 11:31:12 +0300

    1, 2, 3 done

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -16,11 +16,12 @@ void number_of_products_div() /// number of products dividable by smth
     int n(0);
     cin >> n;
     vector<int> numbers(n, 0);
-    for (int i(0); i < n; i++)
-        cin >> numbers[i];
     for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+        cin >> numbers[i];
 
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i < n; i++)
+            cout << numbers[j] << " " << numbers[i] << endl;
 
 }
 
