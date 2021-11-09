866d3363506c199c0c04dc08b7959d96f9e44b47
AlpatovGleb <alpatov31@yandex.ru>
2021-11-09 10:43:26 +0300

    added function

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+void number_of_products_div() /// number of products dividable by smth
+{
+    cout << "Hello, world!" << endl;
+}
+
+
 int main()
 {
     cout << "Hello world!" << endl;
