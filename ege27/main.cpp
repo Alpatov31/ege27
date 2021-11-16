45b95dd2a80f5bddba7e63c63264bf04fa147892
AlpatovGleb <alpatov31@yandex.ru>
2021-11-16 11:22:29 +0300

    reads from file, shows pairs

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 
 using namespace std;
 
 void max_sum_div()
 {
+
+    ifstream ifs("data.txt");
     /**
-    1. считать и сохранить все числа
+    + 1. считать и сохранить все числа
     2. вывести все пары
     3. вместо пары вывести суммы
     4. делимость сумм проверить
     5. вывести только наибольшую сумму
     */
     int n(0);
-    cin >> n;
+    ifs >> n;
     vector<int> numbers(n);
     for (int i(0); i<n; i++)
-        cin >> numbers[i];
+        ifs >> numbers[i];
     cout << endl;
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
-
+    for (int i(0); i<n-1; i++){
+    for (int j(i+1); j<n; j++)
+        cout << numbers[i] << " " << numbers[j] << endl;
+    }
 }
 
 int main()
