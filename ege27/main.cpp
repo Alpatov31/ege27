ec5daf47b1922e1ec9359cb22d9bad24a0ff282b
Alpatov31 <alpatov31@yandex.ru>
2021-11-22 20:17:32 +0300

    reads and saves all numbers

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -7,15 +7,22 @@ using namespace std;
 void max_sum_div()
 {
     /**
-    1. считать и сохранить все числа
+    + 1. считать и сохранить все числа
     2. найти максимальное четное и максимальное нечетное число
     3. найти их сумму
     4. вывести наибольшую нечетную сумму
     */
+    int n(0);
+    int max_even(0);
+    int max_odd(0);
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
