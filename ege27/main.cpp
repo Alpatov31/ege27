590060cf48b50d5943cc6b199d493241823d371c
Alpatov31 <alpatov31@yandex.ru>
2021-11-22 20:48:06 +0300

    added development plan

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void number_sums_div_ineff_2()
+{
+    /**
+    1. считать и запомнить числа
+    2. вывести все пары чисел
+    3. проверить делимость суммы на 40 и наличие элемента больше 40 в паре
+    4. посчитать и вывести итоговое количество
+    */
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_sums_div_ineff_2();
     return 0;
 }
