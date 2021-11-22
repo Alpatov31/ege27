1650a86a977f82f582f5da612e5a1a09d11f8772
Alpatov31 <alpatov31@yandex.ru>
2021-11-22 21:08:34 +0300

    check divisibility by 40 and existence of element that over 40 in the pair

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -8,11 +8,12 @@ void number_sums_div_ineff_2()
     /**
     + 1. считать и запомнить числа. посчитать кол-во элементов в векторе
     + 2. вывести все пары чисел
-    3. проверить делимость суммы на 40 и наличие элемента больше 40 в паре
+    + 3. проверить делимость суммы на 40 и наличие элемента больше 40 в паре
     4. посчитать и вывести итоговое количество
     */
     int n(0);
     int k(0);
+    int kol(0);
     vector<int> numbers;
     while (n != -9999){
         cin >> n;
@@ -22,9 +23,10 @@ void number_sums_div_ineff_2()
     cout << endl;;
     for (int i(0); i<k-2; i++){
     for (int j(i+1); j<k-1; j++)
-        cout << numbers[i] << " " << numbers[j] << endl;
+        if ((numbers[i] + numbers[j]) % 40 == 0)
+            if (numbers[i] > 40 or numbers[j] > 40)
+                kol++;
     }
-
 }
 
 int main()
