d726944abece45fdf12aa8cfcab2662690c39810
AlpatovGleb <alpatov31@yandex.ru>
2021-11-23 11:06:24 +0300

    added development plan

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
+void number_sums_div_eff_2()
+{
+    /**
+    1. считать и сохранить числа
+    2. создать список с количествами остатков от деления на 40
+    3. посчитать количество пар, в которых сумма остатков от деления на 40 кратна 40
+    4. вывести ответ
+    */
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_sums_div_eff_2();
     return 0;
 }
