77891574472cdc7e16eb0cc361aa79441fbf63da
AlpatovGleb <alpatov31@yandex.ru>
2021-11-30 11:56:01 +0300

    reads four numbers

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void min_sum_distance_EFF()
 {
     /**
-    1. считать 4 числа в червяка
+    + 1. считать 4 числа в червяка
     2. число в хвосте червяка - потенциальный минимум из пройденных
     3. сдвинуть все числа в червяке влево (0123 - 1234)
     4. считать пятое число в зубы червяку
@@ -15,10 +15,17 @@ void min_sum_distance_EFF()
     7. это кандидат на минимальную пару
     8. один ход червяка закончен, повторяем
     */
+    int n(0);
+    cin >> n;
+    int min_num(0);
+    int min_sum(0);
+    vector<int> numbers(4);
+    for (int i(0); i < 4; i++)
+        cin >> numbers[i];
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    min_sum_distance_EFF();
     return 0;
 }
