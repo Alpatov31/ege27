8e9aa0076e80a77b0ffb5eac33d1e2b460cb9713
AlpatovGleb <alpatov31@yandex.ru>
2021-11-30 11:38:36 +0300

    finds minimal sum. ineffective solution is ready.

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -10,17 +10,19 @@ void min_sum_distance()
     + 2. вывести пары
     + 3. вывести пары с минимальным расстоянием
     + 4. вместо вывода пары посчитать сумму
-    5. найти минимум среди сумм
+    + 5. найти минимум среди сумм
     */
     int n(0);
+    int min_sum(2001);
     cin >> n;
     vector<int> numbers(n);
     for (int i(0); i<n; i++)
         cin >> numbers[i];
     for (int i(0); i<n-1; i++)
-        for (int j(i+1); j < n; j++)
-            if (i-j >= 4 or j-i >= 4)
-                cout << numbers[i] + numbers[j] << endl;
+        for (int j(i+4); j < n; j++)
+            if ((numbers[i] + numbers[j]) < min_sum)
+                min_sum = numbers[i] + numbers[j];
+            cout << min_sum << endl;
 }
 
 int main()
