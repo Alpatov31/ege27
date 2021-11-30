3a3eea01fc3a25ca09b33ecf358e83292e660b3e
AlpatovGleb <alpatov31@yandex.ru>
2021-11-30 10:47:57 +0300

    reads numbers

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -2,32 +2,20 @@
 
 using namespace std;
 
-void number_sums_div_eff_2()
+void min_sum_distance()
 {
     /**
-    + 1. считать и сохранить числа
-    + 2. создать список с количествами остатков от деления на 40
-    3. посчитать количество пар, в которых сумма остатков от деления на 40 кратна 40
-    4. вывести ответ
+    + 1. считать числа
+    2. вывести пары
+    3. вывести пары с минимальным расстоянием
+    4. вместо вывода пары посчитать сумму
+    5. найти минимум среди сумм
     */
     int n(0);
-    int k(0);
-    int kol(0);
-    vector<int> numbers;
-    vector<int> qttys_by_mod_40(40, 0);
-    while (n != -9999){
-        cin >> n;
-        numbers.push_back(n);
-        k++;
-    }
-    for (int i(0); i<k-1; i++){
-        qttys_by_mod_40[numbers[i]%40]++;}
-    for (int i(0); i<qttys_by_mod_40[i]; i++)
-        cout << i << ":" << qttys_by_mod_40[i] << endl;
-    for (int i(0); i<qttys_by_mod_40.size(); i++){
-        if (qttys_by_mod_40[i] > 0 and qttys_by_mod_40[40-i] > 0)
-            kol += qttys_by_mod_40[i] * qttys_by_mod_40[40-i];}
-    cout << kol << endl;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
 
 }
 
