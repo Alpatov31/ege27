3f04343cdb19dbf26210a0477962df96000a2de5
Alpatov31 <alpatov31@yandex.ru>
2021-12-07 11:40:53 +0300

    reads all numbers

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
+void kol_sum_ineff()
+{
+    /**
+    + 1. считать все числа
+    2. вывести все пары
+    3. найти все пары, у которых разница инедксов равна 6
+    4. найти все пары, сумма которых кратна 25, и посчитать их кол-во
+    5. вывести итоговое кол-во
+    */
+    int n(0);
+    cin >> n;
+    int kol(0);
+    vector<int> numbers(n);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    kol_sum_ineff();
     return 0;
 }
