d33af05e49db405ff21460c1dc18d0a904d8d1f7
Alpatov31 <alpatov31@yandex.ru>
2021-12-20 20:35:09 +0300

    improve effective solution. It shows -1

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -15,18 +15,28 @@ void max_sum_div()
     int n(0);
     int max_even(0);
     int max_odd(0);
+    int kol_odd(0);
+    int kol_even(0);
     int max_sum;
     cin >> n;
     vector<int> numbers(n);
     for (int i(0); i<n; i++)
         cin >> numbers[i];
     for (int i(0); i<n; i++)
-        if (numbers[i] % 2 != 0 and numbers[i] > max_odd)
+        if (numbers[i] % 2 != 0 and numbers[i] > max_odd){
             max_odd = numbers[i];
+            kol_odd++;
+        }
         else if (numbers[i] % 2 == 0 and numbers[i] > max_even)
             max_even = numbers[i];
+            kol_even++;
     max_sum = max_odd + max_even;
-    cout << max_sum << endl;
+    if (kol_even > 0 and kol_odd > 0) {
+
+        cout << max_sum << endl;
+    }
+    else if (kol_even == 0 or kol_odd == 0)
+        cout << -1 << endl;
 }
 
 int main()
