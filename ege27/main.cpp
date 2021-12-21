b7fd64f58bb477a9f58b2f9c6db303e2b8b21bd6
AlpatovGleb <alpatov31@yandex.ru>
2021-12-21 11:00:28 +0300

    added development plan

--- ege27/main.cpp
+++ ege27/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void b()
+{
+    /**
+    1. считать все числа
+    2. заполнить вектор остатками элементов
+    3. посчитать итоговое количество троек
+    */
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    b();
     return 0;
 }
